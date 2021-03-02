d83c82faee4983c3e1402138e80dd30913f71d58
SeonJungKim <ksj083895@gmail.com>
2021-03-02 21:22:42 +0500

    Initial Commit

---
+++ SwiftUI_Alert/SwiftUI_AlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AlertApp.swift
+//  SwiftUI_Alert
+//
+//  Created by 김선중 on 2021/03/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
