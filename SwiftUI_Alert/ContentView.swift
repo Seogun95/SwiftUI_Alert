e2a942f034f8fc65bd791943f1285a046cfb7b01
SeonJungKim <ksj083895@gmail.com>
2021-03-02 21:23:54 +0500

    SwiftUI Alert

--- SwiftUI_Alert/ContentView.swift
+++ SwiftUI_Alert/ContentView.swift
@@ -7,10 +7,45 @@
 
 import SwiftUI
 
+struct AlertId: Identifiable {
+    
+    var id: AlertType
+    
+    enum AlertType {
+        case type1
+        case type2
+    }
+}
+
 struct ContentView: View {
+
+    @State var alertId: AlertId?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button(action: {
+                self.alertId = AlertId(id: .type1)
+            }) {
+                Text("Show alert!")
+            }
+            
+            Button(action: {
+                self.alertId = AlertId(id: .type2)
+            }) {
+                Text("Show alert!")
+            }
+        }.alert(item: $alertId) { (alertId) -> Alert in
+            return createAlert(alertId: alertId)
+        }
+    }
+    
+    private func createAlert(alertId: AlertId) -> Alert {
+        switch alertId.id {
+        case .type1:
+            return Alert(title: Text("Hello World!"))
+        case .type2:
+            return Alert(title: Text("Hello World2!"))
+        }
     }
 }
 
